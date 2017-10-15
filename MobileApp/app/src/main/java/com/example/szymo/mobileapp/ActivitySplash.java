1bbab1b023517d9a0248ee5b09041d2a673e007e
sz <szymon61a@tlen.pl>
2017-10-15 22:44:23 +0200

    permissions view

--- MobileApp/app/src/main/java/com/example/szymo/mobileapp/ActivitySplash.java
+++ MobileApp/app/src/main/java/com/example/szymo/mobileapp/ActivitySplash.java
@@ -1,7 +1,11 @@
 package com.example.szymo.mobileapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,5 +21,26 @@ public class ActivitySplash extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Intent intent = new Intent(this, ActivityPermissions.class);
+                    startActivity(intent);
+                }
+                return;
+            }
+
+        }
     }
 }
