487a1a11f918d2c75aa92ea505fac934155c3bf6
sz <szymon61a@tlen.pl>
2017-10-22 23:31:08 +0200

    add drawer, add menu

--- MobileApp/MobileApp/app/src/main/java/com/example/szymo/mobileapp/ActivityBase.java
+++ MobileApp/MobileApp/app/src/main/java/com/example/szymo/mobileapp/ActivityBase.java
@@ -3,6 +3,7 @@ package com.example.szymo.mobileapp;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -10,14 +11,14 @@ import android.view.WindowManager;
  * Created by szymo on 17.10.2017.
  */
 
-public class ActivityBase extends Activity {
+public class ActivityBase extends AppCompatActivity {
 
     @Override
     public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if (getResources().getBoolean(R.bool.canUseLandscapeMode))
         {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
---
+++ MobileApp/MobileApp/app/src/main/java/com/example/szymo/mobileapp/IActivityAccess.java
@@ -0,0 +1,11 @@
+package com.example.szymo.mobileapp;
+
+import com.example.szymo.mobileapp.controls.FakeCAB;
+
+/**
+ * Created by szymo on 22.10.2017.
+ */
+
+public interface IActivityAccess {
+    FakeCAB accessCAB();
+}
