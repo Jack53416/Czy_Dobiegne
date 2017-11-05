f169bc42ff23db29656b825c7b6825642cf6e332
Quisy <lamprecht.mariusz@gmail.com>
2017-11-05 16:35:26 +0100

    Added map component

--- WebApp/src/scripts/components/App.js
+++ WebApp/src/scripts/components/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Header from './Header';
 import Menu from './Menu';
+import AppMap from './Map';
+import {Grid, Row, Col, Clearfix} from 'react-bootstrap';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <Menu />
-                    <div className="col-xs-12">
-                    
-                    </div>
-                </div>
+            <div className="container-fluid no-padding">
+                <Row className="no-padding">
+                    <Col xs={12} className="no-padding">
+                        <Menu />
+                    </Col>
+                </Row>
+                <Row className="no-padding">
+                    <Col xs={12} className="no-padding">
+                        <AppMap />
+                    </Col>
+                </Row>
             </div>
         );
     }
